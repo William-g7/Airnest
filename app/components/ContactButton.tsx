3a04999e97e4826905d585691970e58bab9c081d
William <william@WilliamdeMacBook-Pro.local>
2024-12-27 13:02:05 -0800

    Implement landlord page, partially hard coded

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+    return (
+        <button className="mt-6 py-4 px-6 w-full bg-airbnb text-white rounded-lg font-semibold hover:bg-airbnb-dark hover:text-white transition-all duration-300">
+            Contact
+        </button>
+    )
+};
+
+export default ContactButton;
