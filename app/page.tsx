f5077d70071c40252024d7576290b8ee50b54c70
William <william@WilliamdeMacBook-Pro.local>
2024-12-27 10:11:39 -0800

    Implement the categories on the front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import Categories from "./components/Categories";
 
 export default function Home() {
   return (
 
     <main className="">
-      <h1>Hello World</h1>
-
-      <h2 className="text-airbnb">Hello World</h2>
+      <main className="max-w-[1500px]mx-auto px-6">
+        <Categories />
+      </main>
     </main>
   );
 }
