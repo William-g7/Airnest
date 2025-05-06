336f5aabd406bdf1540639f489916020498cbbcb
William <william@192.168.4.101>
2025-05-06 12:36:39 +0800

    Update the redirect logic of logo, now will clear all the params in URL

--- djangobnb_frontend/app/components/bar/Navbar.tsx
+++ djangobnb_frontend/app/components/bar/Navbar.tsx
@@ -1,35 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
 import SearchFilters from "./SearchFilters";
 import UserNav from "./UserNav";
 import AddPropertyButton from "./AddPropertyButton";
 import { getUserId } from "@/app/auth/session";
 import LanguageSwitcher from "../LanguageSwitcher";
+import ResetLogo from "./ResetLogo";
 
 const Navbar = async () => {
     const userId = await getUserId();
     return (
-        <nav className="w-full fixed top-0 left-0 py-6 right-0 z-20 bg-white z-10">
-            <div className="max-w-[1500px]mx-auto px-6">
+        <nav className="w-full fixed top-0 left-0 py-5 right-0 z-20 bg-white">
+            <div className="max-w-[1500px] mx-auto px-4 sm:px-6">
                 <div className="flex justify-between items-center">
-                    <Link href="/">
-                        <Image src="/logo.png" alt="logo" width={100} height={38} />
-                    </Link>
+                    <ResetLogo />
 
-                    <div className="flex space-x-4 items-center">
+                    <div className="hidden lg:block flex-1 mx-6 max-w-2xl">
                         <SearchFilters />
                     </div>
 
-                    <div className="flex space-x-4 items-center">
+                    <div className="lg:hidden flex-1 flex justify-center">
+                        <SearchFilters />
+                    </div>
+
+                    <div className="flex items-center space-x-2 sm:space-x-4 flex-shrink-0">
                         <AddPropertyButton />
                         <UserNav userId={userId} />
                         <LanguageSwitcher />
                     </div>
-
                 </div>
             </div>
-
-
         </nav>
     )
 };
---
+++ djangobnb_frontend/app/components/bar/ResetLogo.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter, usePathname } from "next/navigation";
+import { useSearchStore } from "@/app/stores/searchStore";
+
+const ResetLogo = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const { resetFilters } = useSearchStore();
+
+    const handleLogoClick = () => {
+        resetFilters();
+
+        if (window.location.search) {
+            router.replace(pathname);
+        }
+    };
+
+    return (
+        <div className="flex-shrink-0 cursor-pointer" onClick={handleLogoClick}>
+            <Image src="/logo.png" alt="logo" width={100} height={38} />
+        </div>
+    );
+};
+
+export default ResetLogo; 
