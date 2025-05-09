17fc9d86b2ea328a8cfe2bef5b21ee32bfd294ab
William <william@192.168.4.105>
2025-05-09 11:26:45 +0800

    Add a hook to control whether should open the addproperty modal or edit property modal

--- djangobnb_frontend/app/components/hooks/useAddPropertyModal.ts
+++ djangobnb_frontend/app/components/hooks/useAddPropertyModal.ts
@@ -1,13 +1,22 @@
+'use client';
+
 import { create } from "zustand";
+import { PropertyType } from "@/app/constants/propertyType";
 
 interface AddPropertyModalState {
     isOpen: boolean;
+    isEditMode: boolean;
+    propertyToEdit: PropertyType | null;
     onOpen: () => void;
+    onOpenForEdit: (property: PropertyType) => void;
     onClose: () => void;
 }
 
 export const useAddPropertyModal = create<AddPropertyModalState>()((set) => ({
     isOpen: false,
-    onOpen: () => set({ isOpen: true }),
+    isEditMode: false,
+    propertyToEdit: null,
+    onOpen: () => set({ isOpen: true, isEditMode: false, propertyToEdit: null }),
+    onOpenForEdit: (property) => set({ isOpen: true, isEditMode: true, propertyToEdit: property }),
     onClose: () => set({ isOpen: false }),
 }));
