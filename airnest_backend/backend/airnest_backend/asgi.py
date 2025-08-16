da65dc7087db8adcb95e0a60213df309d6260219
William <william@WilliamdeMacBook-Pro.local>
2025-08-16 19:16:34 +0800

    chore(config): update DJANGO_SETTINGS_MODULE from djangobnb.settings to airnest_backend.settings

--- airnest_backend/backend/airnest_backend/asgi.py
+++ airnest_backend/backend/airnest_backend/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobnb_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airnest_backend.settings')
 
 django_asgi_app = get_asgi_application()
 
--- airnest_backend/backend/airnest_backend/wsgi.py
+++ airnest_backend/backend/airnest_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangobnb_backend project.
+WSGI config for airnest_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobnb_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airnest_backend.settings')
 
 application = get_wsgi_application()
