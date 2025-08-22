d8223a5d0a46973f3e4bdff8e35c043a3e681189
William <william@WilliamdeMacBook-Pro.local>
2025-08-22 19:09:55 +0800

    refactor(icons): replace separate SVG components with inline SVG for simpler structure and fewer dependencies

--- airnest_frontend/app/components/properties/PropertyImageCarousel.tsx
+++ airnest_frontend/app/components/properties/PropertyImageCarousel.tsx
@@ -8,9 +8,6 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { useEffect, useState, useCallback, useRef } from 'react';
-import TimesIcon from '../icons/TimesIcon';
-import ChevronLeftIcon from '../icons/ChevronLeftIcon';
-import ChevronRightIcon from '../icons/ChevronRightIcon';
 
 interface ImageCarouselProps {
   images: PropertyImage[];
@@ -211,7 +208,18 @@ const ImageCarousel = ({ images, title }: ImageCarouselProps) => {
               className="text-white bg-black bg-opacity-50 hover:bg-opacity-70 p-3 rounded-full transition-all"
               aria-label="关闭大图"
             >
-              <TimesIcon className="w-6 h-6" />
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </button>
           </div>
 
@@ -221,7 +229,18 @@ const ImageCarousel = ({ images, title }: ImageCarouselProps) => {
               className="text-white bg-black bg-opacity-50 hover:bg-opacity-70 p-3 rounded-full transition-all"
               aria-label="上一张"
             >
-              <ChevronLeftIcon className="w-6 h-6" />
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </button>
           </div>
 
@@ -231,7 +250,18 @@ const ImageCarousel = ({ images, title }: ImageCarouselProps) => {
               className="text-white bg-black bg-opacity-50 hover:bg-opacity-70 p-3 rounded-full transition-all"
               aria-label="下一张"
             >
-              <ChevronRightIcon className="w-6 h-6" />
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </button>
           </div>
 
--- airnest_frontend/app/components/search/EnhancedSearchForm.tsx
+++ airnest_frontend/app/components/search/EnhancedSearchForm.tsx
@@ -7,7 +7,6 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { buildSearchQuery, parseSearchParams } from '@/app/utils/searchParams';
 import { formatDateForAPI, formatLocalizedDate } from '@/app/utils/dateUtils';
 import DatePickerDynamic from '@/app/components/properties/DatePickerDynamic';
-import SearchIcon from '@/app/components/icons/SearchIcon';
 
 /**
  * 增强型搜索表单组件
@@ -350,7 +349,18 @@ export default function EnhancedSearchForm() {
               {isSubmitting ? (
                 <div className="animate-spin w-4 h-4 border border-white border-t-transparent rounded-full"></div>
               ) : (
-                <SearchIcon className="w-4 h-4" />
+                <svg
+                  className="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               )}
             </button>
           </div>
